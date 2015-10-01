78afcb1db90985c8811c3a45b1172aec6e52dff7
Antonio Ricardo <antonio.eschola@gmail.com>
2015-10-01 13:28:51 -0400

    adicionando assets rb flatly

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,4 +3,4 @@ Rails.application.config.assets.version = '1.0'
 # adicionando assets para layout utilizado no controller backend
 Rails.application.config.assets.precompile += %w( flatly.js )
 
-Rails.application.config.assets.precompile += %w( search.css )
+Rails.application.config.assets.precompile += %w( flatly.css )
